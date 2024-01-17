dd40325fb8e36d43bffe4c1d003dfb6dd8241e7e
Ali Hamza <hamzamashooq492@gmail.com>
2024-01-17 22:58:26 +0500

    While Loop

---
+++ test_2/index.js
@@ -0,0 +1,8 @@
+// 2. Write a ts program to print all natural numbers 
+// in reverse (from n to 1). - using while loop
+
+var n=prompt("enter a number:")
+while (n>=1) {
+    console.log(n);
+    n--;
+}
---
+++ test_3/index.js
@@ -0,0 +1,8 @@
+// Write a ts program to print all alphabets from a to z.
+//  - using while loop
+var i=97
+while (i<=122) {
+    var b=String.fromCharCode(i)
+    console.log(b);
+    i++;
+}
