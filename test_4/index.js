a43593974757a1636e04e2929bb2cd28db686ab5
Ali Hamza <hamzamashooq492@gmail.com>
2024-01-24 23:15:16 +0500

    while loop

---
+++ test_4/index.js
@@ -0,0 +1,7 @@
+// 4. Write a ts program to print all even numbers
+//  between 1 to 100. - using while loop
+var i=2
+while (i<=100) {
+    console.log(i);
+    i+= 2;
+}
---
+++ test_5/index.js
@@ -0,0 +1,7 @@
+// 5. Write a ts program to print all odd number
+//  between 1 to 100.
+var i=1
+while (i<=100) {
+    console.log(i);
+    i+=2;
+}
