9e4a48207724df65cd44b0557c5653338e5ced91
Ali Hamza <hamzamashooq492@gmail.com>
2024-01-24 23:16:17 +0500

    For loop

---
+++ test_6/index.js
@@ -0,0 +1,8 @@
+// 6. Write a ts program to find sum of all
+//  natural numbers between 1 to n.
+var n=parseInt(prompt("enter a number"))
+var sum=0
+for (i=1; i<=n; i++){
+    sum=sum +i
+}
+console.log(sum);
